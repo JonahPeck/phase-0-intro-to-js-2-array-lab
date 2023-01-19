caf7bea4ba85cfb7b77cedbf559ffd441d9b0667
JonahPeck <PeckJonahC@gmail.com>
2023-01-19 12:31:25 -0700

    commit tree 32823326e903bc6ad954d0f757a50322074d681c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+cats;
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats;
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    return cats;
+}
+
+function appendCat (name) {
+    const appendCat = [...cats, name];
+    return appendCat;
+}
+function prependCat (name) {
+    const prependCat = [name, ...cats];
+    return prependCat;
+}
+function removeLastCat (name) {
+    const removeLastCat = cats.slice(name, -1);
+    return removeLastCat;
+}
+
+function removeFirstCat (name) {
+    const removeFirstCat = cats.slice(1, name);
+    return removeFirstCat;
+}
